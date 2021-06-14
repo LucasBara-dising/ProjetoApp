fc2eae7914778ae1b6fcb484582eb13e3183cc98
LucasBara-dising <lucas.barauna098@gmail.com>
2021-06-14 14:30:06 -0300

    Initial commit

---
+++ app/src/main/java/com/example/projetoapp/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.projetoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
+
+public class MainActivity extends AppCompatActivity {
+    private Button BtnTelaBemVindo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BtnTelaBemVindo= (Button) findViewById(R.id.BtnTelaBemVindo);
+        BtnTelaBemVindo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Activity2();
+            }
+        });
+    }
+   public void Activity2(){
+       Intent intent = new Intent(this, Activity2.class);
+       startActivity(intent);
+   }
+}
