6a0041454a409e51920bb551be445b053d3e5ed1
LucasBara-dising <lucas.barauna098@gmail.com>
2021-06-23 20:15:52 -0300

    Criação da tela de Historico de reserva

--- app/src/main/java/com/example/projetoapp/Home.java
+++ app/src/main/java/com/example/projetoapp/Home.java
@@ -5,22 +5,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Home extends AppCompatActivity {
     Button BtnVerMais;
     Button BtnCriarLogin;
-
     ImageButton BtnFiltro;
-
-    ImageView BtnHistorico;
-    TextView txtViewHistorico;
-
-    ImageView btnConta;
-    TextView txtViewConta;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,45 +38,7 @@ public class Home extends AppCompatActivity {
             }
         });
 
-        //Para tela historico
-        BtnHistorico=(ImageView) findViewById(R.id.BtnHistorico);
-        BtnHistorico.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick (View v){
-                Intent historico = new Intent(getApplicationContext(), HistoricoDeReserva.class);
-                startActivity(historico);
-            }
-        });
-        txtViewHistorico=(TextView) findViewById(R.id.txtViewHistorico);
-        txtViewHistorico.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick (View v){
-                Intent historico = new Intent(getApplicationContext(), HistoricoDeReserva.class);
-                startActivity(historico);
-            }
-        });
-
-        //Para tela conta
-        btnConta=(ImageView) findViewById(R.id.btnConta);
-        btnConta.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick (View v){
-                Intent conta = new Intent(getApplicationContext(), Conta.class);
-                startActivity(conta);
-            }
-        });
-        txtViewConta=(TextView) findViewById(R.id.txtViewConta);
-        txtViewConta.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick (View v){
-                Intent conta = new Intent(getApplicationContext(), Conta.class);
-                startActivity(conta);
-            }
-        });
-
-
-        //para tela filtro
-        BtnFiltro=(ImageButton) findViewById(R.id.BtnFiltro);
+        BtnFiltro=(ImageButton) findViewById(R.id.imgBtnFiltro);
         BtnFiltro.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick (View v){
